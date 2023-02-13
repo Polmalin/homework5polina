22ac9796d71eed05965ae7004d8fa3df3982971c
Polina <polinazholudeva511256@gmail.com>
2023-02-13 10:03:08 +0300

    feat:task1 to homework5

---
+++ task1/Program.cs
@@ -0,0 +1,42 @@
+﻿//Задача 34: Задайте массив заполненный случайными положительными трёхзначными числами. Напишите программу, которая покажет количество чётных чисел в массиве.
+
+//[345, 897, 568, 234] -> 2
+
+int [] arr = InitArray();
+PrintArray(arr);
+int result =  getEvenNumber(arr);
+Console.WriteLine ($"В массие {result} четных чисел");
+int[] InitArray()
+{
+    int[] result = new int[12];
+    Random rnd = new Random();
+    for (int i = 0; i < result.Length; i++)
+    {
+        result[i] = rnd.Next(100,1000);
+    }
+    return result;
+}
+
+void PrintArray(int[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.Write($"{array[i]} ");
+    }
+    Console.WriteLine();
+}
+
+int getEvenNumber(int[] array)
+{
+    int ammount = 0;
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (array [i] % 2 == 0)
+        {
+            ammount++;
+        }
+    }
+    return ammount;
+}
+
+
