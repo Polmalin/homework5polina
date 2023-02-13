3dab70d3183d62b71540d71a35cb257f45f69dcf
Polina <polinazholudeva511256@gmail.com>
2023-02-13 10:27:57 +0300

    feat:task3 to homework5

---
+++ task3/Program.cs
@@ -0,0 +1,45 @@
+﻿//  Задайте массив вещественных чисел. Найдите разницу между максимальным и минимальным элементов массива.
+//[3 7 22 2 78] -> 76
+
+double [] arr = InitArray();
+PrintArray(arr);
+double result =  getDifOfMinMax(arr);
+Console.WriteLine ($"Разница между максимальным и минимальным элементом массива = {result}");
+
+double [] InitArray()
+{
+    double [] result = new double[12];
+    Random rnd = new Random();
+    for (int i = 0; i < result.Length; i++)
+    {
+        result[i] = rnd.NextDouble();
+    }
+    return result;
+}
+void PrintArray(double [] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.Write($"{array[i]} ");
+    }
+    Console.WriteLine();
+}
+
+double getDifOfMinMax(double[] array)
+{
+    double max = array [0];
+    double min = array [0];
+    for (int i = 1; i < array.Length; i++)
+    {
+        if (array [i] > max)
+        {
+            max=array [i];
+        }
+        else if (array [i] < min)
+        {
+            min=array [i];
+        }
+    }
+    return (max-min);
+    
+}
